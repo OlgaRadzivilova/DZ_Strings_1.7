a36079318d568b84febb0f71303508332e5d8b6d
OlgaRadzivilova <radzivilovaolga@gmail.com>
2022-06-02 18:29:55 +0300

    1 commit Homework_1.7 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+
+// Задание 1
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        {
+            String fullName = "Ivanov " + "Ivan " + "Ivanovich";
+
+            System.out.println("Employee`s full name - " + fullName);
+
+
+// Задание 2
+
+            String fullNAME = fullName.toUpperCase();
+
+            System.out.println("Employee`s full name data for filling out the report - " + fullNAME);
+
+        }
+
+// Задание 3
+
+        String fullName = "Ivanov Semёn Semёnovich";
+
+        fullName = fullName.replace('ё', 'e');
+
+            System.out.println("Employee's full name data - " + fullName);
+
+        }
+
 }
+
+
+
+
+
+
+
+
+
